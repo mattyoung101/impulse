29212df3083f2d63ecaad544399eff32318c287b
Matt Young <matt.young.1@outlook.com>
2022-10-14 16:25:25 +1000

    Added spectogram output

---
+++ rtl/osc_noise.sv
@@ -0,0 +1,6 @@
+// This file is part of Impulse, the retro FPGA synthesizer
+// Copyright (c) 2022 Matt Young. All rights reserved.
+
+// Noise oscillator for Impulse
+// This oscillator is based on a 16-bit maximal-period LFSR, sourced from:
+// https://en.wikipedia.org/wiki/Linear-feedback_shift_register#Example_polynomials_for_maximal_LFSRs
